9d9d79955e4054be8fb165e163b53995916dea10
Richard Wells <rwells1703@gmail.com>
2022-01-04 01:31:15 +0000

    Run different class methods depending on the URL

--- index.php
+++ index.php
@@ -3,20 +3,17 @@
 require 'vendor/autoload.php';
 $f3 = \Base::instance();
 
-class Cat {
-    function speak($f3,$params) {
-        $name = $params['name'];
-         
-        if ($name == "Sam") {
-            echo 'MEOW';
-        } else if ($name == "Lily") {
-            echo 'MEEEEEOOOOOOWOWOWOWOW';
-        } else {
-            echo 'IM NOT A CAT';
-        }
+class Colors {
+    function red() {
+        echo "The color of fire";
+    }
+
+    function blue() {
+        echo "The color of the sea";
     }
 }
-$f3->route('GET @cat_speak: /cat/@name', 'Cat->speak');
+
+$f3->route('GET /colors/@color', 'Colors->@color');
 
 $f3->run();
 
